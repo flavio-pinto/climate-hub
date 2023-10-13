8674708e208285b83bf2f943f20e723a6e499d2b
Flavio Pinto <flaviopnt@hotmail.it>
2023-10-13 19:02:47 +0200

    [RESET] Project reset

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,12 @@ import {
 import "normalize.css";
 import "bootstrap/dist/css/bootstrap-grid.min.css";
 import "./App.css";
-import Header from "./components/Header/Header";
-import Sidebar from "./components/Sidebar/Sidebar";
 import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <div className="grid-container">
       <Router>
-        <Header />
-        <Sidebar />
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>

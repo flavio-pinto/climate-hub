82dc512b99c266a6026e9f6d683b53b7b8ec3c25
Flavio Pinto <flaviopnt@hotmail.it>
2023-10-13 17:50:12 +0200

    [FEAT] Sidebar basic structure and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,27 @@
-import 'normalize.css'
-import 'bootstrap/dist/css/bootstrap-grid.min.css'
-import './App.css'
-import Header from './components/Header/Header'
-import Sidebar from './components/Sidebar/Sidebar'
-import Home from './pages/Home/Home'
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
+import "normalize.css";
+import "bootstrap/dist/css/bootstrap-grid.min.css";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
-    <div className='grid-container'>
-      <Header />
-      <Sidebar />
-      <Home />
+    <div className="grid-container">
+      <Router>
+        <Header />
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;

85002771c32bae6b295b54b6a7068696a1e6bca3
Flavio Pinto <flaviopnt@hotmail.it>
2023-10-13 12:53:59 +0200

    [FEAT] Header basic structure and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import 'normalize.css'
 import 'bootstrap/dist/css/bootstrap-grid.min.css'
 import './App.css'
+import Header from './components/Header/Header'
+import Sidebar from './components/Sidebar/Sidebar'
+import Home from './pages/Home/Home'
 
 function App() {
   return (
-    <h1>ClimateHub</h1>
+    <div className='grid-container'>
+      <Header />
+      <Sidebar />
+      <Home />
+    </div>
   )
 }
 

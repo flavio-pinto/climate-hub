bb80baeaf60510df157b2e11fd217ecd6f2535e2
Flavio Pinto <flaviopnt@hotmail.it>
2023-10-15 19:46:59 +0200

    [FEAT] Toggle dark mode

--- src/component/Header/Header.tsx
+++ src/component/Header/Header.tsx
@@ -1,36 +1,42 @@
-import { BiChevronLeft } from "react-icons/bi"
-import { BsFillSunFill, BsFillMoonStarsFill, BsGithub, BsLinkedin, BsCodeSquare } from "react-icons/bs"
-import styles from "./Header.module.css"
+import { BiChevronLeft } from "react-icons/bi";
+import { BsGithub, BsLinkedin, BsCodeSquare } from "react-icons/bs";
+import Toggle from 'react-toggle'
+import 'react-toggle/style.css'
+import './DarkModeToggleOverride.css'
+import styles from "./Header.module.css";
 
 type Props = {
-  darkMode: boolean
-  setDarkMode: React.Dispatch<React.SetStateAction<boolean>>
-}
+  darkMode: boolean;
+  setDarkMode: React.Dispatch<React.SetStateAction<boolean>>;
+};
 
 const Header: React.FC<Props> = (props: Props) => {
+  const toggleDarkMode = () => {
+    props.setDarkMode(!props.darkMode);
+  };
+
   return (
-    <header className={`${styles.header} d-flex justify-content-between align-items-center px-1 py-2`}>
+    <header
+      className={`${styles.header} d-flex justify-content-between align-items-center px-1 py-2`}
+    >
       <div className={`${styles.headerLeft} d-flex align-items-center`}>
         <BiChevronLeft />
-        {/* <BsFillSunFill />
-        <BsFillMoonStarsFill /> */}
-        {props.darkMode ? (
-          <BsFillMoonStarsFill onClick={() => props.setDarkMode(false)} />
-        ) : (
-          <BsFillSunFill onClick={() => props.setDarkMode(true)} />
-        )}
+        <Toggle
+          checked={props.darkMode}
+          onChange={toggleDarkMode}
+          icons={{ checked: "🌙", unchecked: "🔆" }}
+          aria-label="Dark mode toggle"
+          className={`${styles.toggleIcons}`}
+        />
       </div>
-      <div className={`${styles.headerCenter}`}>
-        Logo
-      </div>
+      <div className={`${styles.headerCenter}`}>Logo</div>
       <div className={`${styles.headerRight} d-flex align-items-center`}>
         <BsGithub />
         <BsLinkedin />
         <BsCodeSquare />
       </div>
-      
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;

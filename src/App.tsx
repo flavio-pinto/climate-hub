1e55128355b37ff9e5df5e395bc04b9b4db0bf54
Flavio Pinto <flaviopnt@hotmail.it>
2023-10-15 17:37:40 +0200

    [FEAT] Button switch light/dark mode (styles need to be fixed)

--- src/App.tsx
+++ src/App.tsx
@@ -9,15 +9,27 @@ import "./App.css";
 import Home from "./pages/Home/Home";
 import Sidebar from "./component/Sidebar/Sidebar";
 import Header from "./component/Header/Header";
+import { useState } from "react";
 
 function App() {
+  // Aggiungi uno stato per gestire la modalità chiara o scura
+  const [darkMode, setDarkMode] = useState<boolean>(false);
+
+  // Aggiungi un gestore di eventi per il click sull'icona del sole
+  /* const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+    // Aggiorna la classe del corpo del documento
+    document.body.classList.toggle('dark-mode', !darkMode);
+  }; */
+
   return (
-    <div className="app">
+    <div className={`app ${darkMode ? 'dark-mode' : ''}`}>
       <Router>
+        {/* <div className="d-flex appContainer"> */}
         <div className="d-flex appContainer">
           <Sidebar />
           <div className="appContent">
-            <Header />
+            <Header setDarkMode={setDarkMode} darkMode={darkMode} />
             <Routes>
               <Route path="/" element={<Home />} />
             </Routes>
--- src/component/Header/Header.tsx
+++ src/component/Header/Header.tsx
@@ -1,15 +1,24 @@
 import { BiChevronLeft } from "react-icons/bi"
-import { BsFillSunFill, /* BsFillMoonStarsFill, */ BsGithub, BsLinkedin, BsCodeSquare } from "react-icons/bs"
+import { BsFillSunFill, BsFillMoonStarsFill, BsGithub, BsLinkedin, BsCodeSquare } from "react-icons/bs"
 import styles from "./Header.module.css"
-/* type Props = {} */
 
-const Header: React.FC = (/* props: Props */) => {
+type Props = {
+  darkMode: boolean
+  setDarkMode: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+const Header: React.FC<Props> = (props: Props) => {
   return (
     <header className={`${styles.header} d-flex justify-content-between align-items-center px-1 py-2`}>
       <div className={`${styles.headerLeft} d-flex align-items-center`}>
         <BiChevronLeft />
-        <BsFillSunFill />
-        {/* <BsFillMoonStarsFill /> */}
+        {/* <BsFillSunFill />
+        <BsFillMoonStarsFill /> */}
+        {props.darkMode ? (
+          <BsFillMoonStarsFill onClick={() => props.setDarkMode(false)} />
+        ) : (
+          <BsFillSunFill onClick={() => props.setDarkMode(true)} />
+        )}
       </div>
       <div className={`${styles.headerCenter}`}>
         Logo

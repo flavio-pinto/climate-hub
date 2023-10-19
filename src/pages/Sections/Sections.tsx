e90664edf53c7dc555985861a996153f75693519
Flavio Pinto <flaviopnt@hotmail.it>
2023-10-19 14:49:02 +0200

    [FEAT] Section data (no chart atm)

--- src/pages/Sections/Sections.tsx
+++ src/pages/Sections/Sections.tsx
@@ -1,6 +1,8 @@
 import { useParams } from "react-router-dom";
+import styles from "./Sections.module.css";
 import { temperature, co2, methane, no2, ice } from "../../data/section-data";
 import { SectionInfo } from "../../interfaces/SectionInfo";
+import { Col, Container, ListGroup, Row } from "react-bootstrap";
 
 const sectionDataMap: Record<string, SectionInfo> = {
   temperature,
@@ -24,14 +26,43 @@ const Sections: React.FC = () => {
   const sectionData = sectionDataMap[section];
 
   return (
-    <main className="content">
-      <h1>{sectionData.name}</h1>
-      <p>{sectionData.description}</p>
-      <ul>
+    <main className={`${styles.content} d-flex p-4`}>
+      {/* <ul>
         {sectionData.whatCanWeDo.map(data => (
           <li>{data}</li>
         ))}
-      </ul>
+      </ul> */}
+      <Container>
+        <Row className="align-items-center">
+          <Col xs={12} lg={8} className={styles.contentDescription}>
+            <h2>{sectionData.name}</h2>
+            <p>{sectionData.description}</p>
+          </Col>
+          <Col xs={12} lg={4} >
+            {/* <ListGroup>
+              <h3>What Can We Do?</h3>
+              {sectionData.whatCanWeDo.map((data) => (
+                <ListGroup.Item>{data}</ListGroup.Item>
+              ))}
+            </ListGroup> */}
+            <img
+              src="https://images.vexels.com/media/users/3/298390/isolated/lists/f659981c21d381aba498ac17f44d5b5d-global-warming-planet-earth-cartoon-character.png"
+              alt=""
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={12} lg={4}>
+            <ListGroup>
+              <h3>What Can We Do?</h3>
+              {sectionData.whatCanWeDo.map((data) => (
+                <ListGroup.Item>{data}</ListGroup.Item>
+              ))}
+            </ListGroup>
+          </Col>
+          <Col></Col>
+        </Row>
+      </Container>
     </main>
   );
 };

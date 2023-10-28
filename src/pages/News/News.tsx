6d8c22df68b024fe1054e062169b69a3425fd4be
Flavio Pinto <flaviopnt@hotmail.it>
2023-10-28 13:22:10 +0200

    [FEAT] News rendering

--- src/pages/News/News.tsx
+++ src/pages/News/News.tsx
@@ -1,15 +1,41 @@
+import { ListGroup } from "react-bootstrap";
+import useNewsData from "../../services/fetchNews";
+import styles from "./News.module.css";
+import { Link } from "react-router-dom";
+import { Article } from "../../interfaces/Article";
+import { FaNewspaper } from "react-icons/fa6";
+
 /* type Props = {} */
 
-import useNewsData from "../../services/fetchNews"
-
 const News: React.FC = (/* props: Props */) => {
-  const { news, error } = useNewsData()
-  console.log(news)
-  
-  
-  return (
-    <div>News</div>
-  )
-}
+  const { news, error } = useNewsData();
 
-export default News
+  return (
+    <main className={`${styles.news} p-4`}>
+      <h2>
+        Latest News <small>(powered by <a href="https://newyorktimesclonebyflavio.netlify.app/" target="_blank">The New York Times Clone</a>)</small>
+      </h2>
+      <ListGroup className={`${styles.newsContentList} mb-5`}>
+        {news.map((article: Article) => (
+          <>
+            <Link
+              className={`${styles.newsContentItem} d-block py-3`}
+              to={article.url}
+              target="_blank"
+            >
+              <ListGroup.Item>
+                <h3><FaNewspaper className="me-2" />{article.title}</h3>
+                <p>{article.abstract}</p>
+                <small>{article.byline}</small>
+              </ListGroup.Item>
+            </Link>
+            <hr />
+          </>
+        ))}
+      </ListGroup>
+      <a className={styles.more} href="https://newyorktimesclonebyflavio.netlify.app/search/climate" target="_blank">Want more news on climate change? Check out my clone of The New York Times!</a>
+    </main>
+  );
+};
+
+export default News;
